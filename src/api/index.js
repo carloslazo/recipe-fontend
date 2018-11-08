05c8292204280842b497c67f9205999feea1ed21
Carlos Lazo <clazo@berkeley.edu>
2018-11-08 00:04:02 -0800

    made changes

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import AuthService from "../services/AuthService";
 
-const DOMAIN = "https://recipe-app-text.herokuapp.com/"
+const DOMAIN = "https://salty-shelf-93882.herokuapp.com/"
 // const DOMAIN = "http://localhost:3001";
 const Auth = new AuthService();
 
